9c6908596a28996f14ea76ce6bf1642616344dd5
liqi <qli@qeelyn.hk>
2024-12-12 16:53:45 +0800

    增加代码注释

--- api/oas/auth/impl.go
+++ api/oas/auth/impl.go
@@ -91,14 +91,14 @@ type Options struct {
 	PwdPolicy OptionsPwdPolicy `json:"pwdPolicy"`
 }
 type OptionsPwdPolicy struct {
-	Length               int32 `json:"length"`
-	IncludeElement       int32 `json:"includeElement"`
-	IncludeChar          int32 `json:"includeChar"`
-	AllowIncludeUserName bool  `json:"allowIncludeUserName"`
-	InvalidDay           int32 `json:"invalidDay"`
-	InvalidLoginLimit    bool  `json:"invalidLoginLimit"`
-	Retry                int32 `json:"retry"`
-	CaptchaTimes         int32 `json:"captchaTimes"`
+	Length               int32 `json:"length"`               // 密码最短长度，长度应在6-32位之间
+	IncludeElement       int32 `json:"includeElement"`       // 必须包含的元素，异或：1-小写字母，2-大写字母，4-数字，8-符号
+	IncludeChar          int32 `json:"includeChar"`          // 最少包含的不同字符数，最多8个，0代表不限制
+	AllowIncludeUserName bool  `json:"allowIncludeUserName"` // 是否允许包含用户名
+	InvalidDay           int32 `json:"invalidDay"`           // 有效天数，最大1095天，0代表不过期
+	InvalidLoginLimit    bool  `json:"invalidLoginLimit"`    // 过期后是否限制登录
+	Retry                int32 `json:"retry"`                // 一小时内密码错误最多尝试次数，最大32次，0代表不限次数
+	CaptchaTimes         int32 `json:"captchaTimes"`         // 密码错误多少次出现验证码，最大5次，0代表不出现验证码
 }
 
 // ServerImpl is the server API for service.
@@ -188,12 +188,14 @@ func (s *ServerImpl) Captcha(ctx *gin.Context, req *CaptchaRequest) (*Captcha, e
 func (s *ServerImpl) Login(ctx *gin.Context, req *LoginRequest) (res *LoginResponse, err error) {
 	var failCount int32
 	s.cache.Get(ctx, loginFailCachePrefix+req.Username, &failCount)
+	// 获取密码策略
 	upp, err := s.getPasswordPolicy(ctx)
 	if err != nil {
 		ctx.Status(http.StatusBadRequest)
 		return nil, err
 	}
 	if upp.CaptchaTimes > 0 && failCount >= upp.CaptchaTimes {
+		// 若错误次数大于验证码应该出现的次数，则前端展示验证码
 		if req.CaptchaId == "" || req.Captcha == "" {
 			return &LoginResponse{CallbackUrl: callBackUrlCaptcha}, nil
 		}
@@ -509,6 +511,7 @@ func (s *ServerImpl) checkPwd(ctx *gin.Context, req *LoginRequest, upp *ent.User
 		var failCount int32
 		s.cache.Get(ctx, loginFailCachePrefix+req.Username, &failCount)
 		if upp.Retry > 0 && failCount >= upp.Retry && upp.InvalidLoginLimit {
+			// 密码错误次数大于重试次数，且限制登录，则返回登录限制
 			ctx.Status(http.StatusForbidden)
 			return nil, status.ErrLoginFailUpperLimit
 		}
--- service/resource/service.go
+++ service/resource/service.go
@@ -9,14 +9,14 @@ import (
 type Option func(*Service)
 
 type PwdPolicy struct {
-	Length               int32 `json:"length"`
-	IncludeElement       int32 `json:"includeElement"`
-	IncludeChar          int32 `json:"includeChar"`
-	AllowIncludeUserName bool  `json:"allowIncludeUserName"`
-	InvalidDay           int32 `json:"invalidDay"`
-	InvalidLoginLimit    bool  `json:"invalidLoginLimit"`
-	Retry                int32 `json:"retry"`
-	CaptchaTimes         int32 `json:"captchaTimes"`
+	Length               int32 `json:"length"`               // 密码最短长度，长度应在6-32位之间
+	IncludeElement       int32 `json:"includeElement"`       // 必须包含的元素，异或：1-小写字母，2-大写字母，4-数字，8-符号
+	IncludeChar          int32 `json:"includeChar"`          // 最少包含的不同字符数，最多8个，0代表不限制
+	AllowIncludeUserName bool  `json:"allowIncludeUserName"` // 是否允许包含用户名
+	InvalidDay           int32 `json:"invalidDay"`           // 有效天数，最大1095天，0代表不过期
+	InvalidLoginLimit    bool  `json:"invalidLoginLimit"`    // 过期后是否限制登录
+	Retry                int32 `json:"retry"`                // 一小时内密码错误最多尝试次数，最大32次，0代表不限次数
+	CaptchaTimes         int32 `json:"captchaTimes"`         // 密码错误多少次出现验证码，最大5次，0代表不出现验证码
 }
 
 // Service 企业目录服务管理
